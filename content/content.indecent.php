90946c6927db3e6fec59f90feffa7ab7f5acb647
Brendan Abbott <brendan@bloodbone.ws>
2012-03-28 22:54:55 +1000

    Update for 2.3 compatibility

--- content/content.indecent.php
+++ content/content.indecent.php
@@ -3,61 +3,49 @@
 	require_once(TOOLKIT . '/class.administrationpage.php');
 
 	Class contentExtensionIndecentIndecent extends AdministrationPage {
-		protected $_driver;
-
-		function __construct(&$parent){
-			parent::__construct($parent);
-			$this->setTitle(__('Symphony &ndash; Indecent Filter'));
-		}
 
 		public function view(){
-			$this->_driver = $this->_Parent->ExtensionManager->create('indecent');
-
+			$this->setTitle(__('Symphony &ndash; Indecent Filter'));
 			$this->setPageType('form');
 			$this->Form->setAttribute('enctype', 'multipart/form-data');
 			$this->appendSubheading(__('Indecent Filter'));
+			$words = extension_indecent::processFilterList(true);
 
-			## Word List
+			// Word List
+			$container = new XMLElement('fieldset');
+			$container->setAttribute('class', 'settings');
+			$container->appendChild(
+				new XMLElement('legend', __('Word Blacklist'))
+			);
 
-				$container = new XMLElement('fieldset');
-				$container->setAttribute('class', 'settings');
-				$container->appendChild(
-					new XMLElement('legend', __('Word Blacklist'))
-				);
+			$p = new XMLElement('p', __('Enter each of the terms on a new line.'), array(
+				'class' => 'help'
+			));
+			$container->appendChild($p);
 
-				$p = new XMLElement('p', __(sprintf(
-					'Enter each of the terms on a new line. <br /> This list was last updated on %s.',
-					$this->_driver->lastUpdateFilterList()
-					))
-				);
-				$p->setAttribute('class', 'help');
-				$container->appendChild($p);
+			// Add last saved timestamp
+			if(!empty($words)) {
+				$p->setValue($p->getValue() . '<br / >' . __('Last updated: %s', array(
+					extension_indecent::lastUpdateFilterList()
+				)));
+			}
 
-				$group = new XMLElement('div');
-				$group->setAttribute('class', 'group');
-				$group->appendChild(Widget::Textarea('fields[words]',30,30, $this->_driver->processFilterList(true)));
-
-			$container->appendChild($group);
+			$container->appendChild(
+				Widget::Textarea('fields[words]',30, 30, $words)
+			);
 			$this->Form->appendChild($container);
 
-			## Actions
-
-				$div = new XMLElement('div');
-				$div->setAttribute('class', 'actions');
-
-				$attr = array('accesskey' => 's');
-				$div->appendChild(Widget::Input('action[save]', 'Save', 'submit', $attr));
+			// Actions
+			$div = new XMLElement('div');
+			$div->setAttribute('class', 'actions');
+			$div->appendChild(Widget::Input('action[save]', 'Save', 'submit', array('accesskey' => 's')));
 
 			$this->Form->appendChild($div);
 		}
 
 		public function __actionIndex() {
-			if (empty($this->_driver)) {
-				$this->_driver = $this->_Parent->ExtensionManager->create('indecent');
-			}
-
 			if (isset($_POST['action']['save'])) {
-				$state = $this->_driver->saveFilterList($_POST['fields']['words']);
+				$state = extension_indecent::saveFilterList($_POST['fields']['words']);
 			}
 
 			if($state !== FALSE) {
@@ -65,7 +53,8 @@
 					__("Indecent Filter List was updated successfully"),
 					Alert::SUCCESS
 				);
-			} else {
+			}
+			else {
 				$this->pageAlert(
 					__("There was an error saving the Indecent Filter List"),
 					Alert::ERROR
@@ -73,5 +62,3 @@
 			}
 		}
 	}
-
-?>
